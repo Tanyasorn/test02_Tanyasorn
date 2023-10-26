85c7005b1d609fc619f06fb444487564c8f354dc
Tanyasorn <test.tanyasorn.com>
2023-10-27 00:12:13 +0700

    add feature4 and edit feature name

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function feature2() {
     alert('from feature 2.');
 }
 
-function feature2() {
+function feature3() {
     alert('from feature 3.');
 }
+
+function feature4() {
+    alert('from feature 4.');
+}
+//save
